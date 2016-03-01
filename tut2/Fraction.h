4b1cd46656d757bf6f5325fca0c8e28e12e67408
Masienyane12 <prince.masienyane@gmail.com>
2016-03-01 10:31:14 -0800

    added member fubctions

--- tut2/Fraction.h
+++ tut2/Fraction.h
@@ -7,16 +7,18 @@ class Fraction
 public:
 	Fraction();                        //constructor
 	~Fraction();                       //destructor
-	void getNumDenom(,)                // get denominator and numerator   function
-		void setNumDenom(,)            //  
+	void getNumDenom(int,int,int,int)                // get denominator and numerator   function
+	void setNumDenom(int,int,int,int )            //  
 
+		void add();                  
+	void subtract();
+	void multiply();
+	void divide();   
 
-
-
-
-
-
-
+private :
+	int numer;
+	int denom;
 
 
 };
+#endif
