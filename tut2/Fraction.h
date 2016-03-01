b724825c555c085a0a68021969ae3621337faf23
Masienyane12 <prince.masienyane@gmail.com>
2016-03-01 09:48:24 -0800

    1st commit-creat header

---
+++ tut2/Fraction.h
@@ -0,0 +1,22 @@
+# ifndef FRACTION_H
+# define FRACTION_H
+
+class Fraction
+{
+
+public:
+	Fraction();                        //constructor
+	~Fraction();                       //destructor
+	void getNumDenom(,)                // get denominator and numerator   function
+		void setNumDenom(,)            //  
+
+
+
+
+
+
+
+
+
+
+};
